114241069a408cc6ff299dcaca9b94b6431ed3da
Andrew Kicheev <kicheevandrey@gmail.com>
2020-04-06 19:04:31 +0300

    Print amount of ingredients

--- coffee_machine.py
+++ coffee_machine.py
@@ -6,6 +6,11 @@ status_of_making_coffee = ['Starting to make a coffee',
                  'Pouring some milk into the cup',
                  'Coffee is ready!']
 
-for i in status_of_making_coffee:
-    print(i)
-    
+one_cup_composition = {'water': (200, 'ml'),
+                       'milk': (50, 'ml'),
+                       'coffee beans': (15, 'g')}
+
+cups_amount = int(input('Write how many cups of coffee you will need:\n'))
+print('For 25 cups of coffee you will need:')
+for key, value in one_cup_composition.items():
+    print(value[0] * cups_amount, value[1], 'of', key)
