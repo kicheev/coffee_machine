c8ce003827f01a19277c76b755499a0390269393
Andrew Kicheev <kicheevandrey@gmail.com>
2020-04-10 18:16:47 +0300

    Class attribute form of supply.

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,14 +1,20 @@
 class CoffeeMachine:
-    coffee_types = {'espresso': {'water': 250, 'coffee beans': 16, 'disposable cups': 1, 'money': -4},
-                    'latte': {'water': 350, 'milk': 75, 'coffee beans': 20, 'disposable cups': 1, 'money': -7},
-                    'cappuccino': {'water': 200, 'milk': 100, 'coffee beans': 12, 'disposable cups': 1, 'money': -6}}
+    coffee_types = {'espresso': {'water': 250, 'c_beans': 16, 'd_cups': 1, 'money': -4},
+                    'latte': {'water': 350, 'milk': 75, 'c_beans': 20, 'd_cups': 1, 'money': -7},
+                    'cappuccino': {'water': 200, 'milk': 100, 'c_beans': 12, 'd_cups': 1, 'money': -6}}
 
     def __init__(self, water, milk, c_beans, d_cups, money):
-        self.supply = {'water': water,
-                       'milk': milk,
-                       'coffee beans': c_beans,
-                       'disposable cups': d_cups,
-                       'money': money}
+        self.water = water
+        self.milk = milk
+        self.c_beans = c_beans
+        self.d_cups = d_cups
+        self.money = money
+
+        self.attribute_keys = {'water': 'water',
+                               'milk': 'milk',
+                               'c_beans': 'coffee beans',
+                               'd_cups': 'disposable cups',
+                               'money': 'money'}
 
     def user_input(self):
         return input()
@@ -34,8 +40,8 @@ class CoffeeMachine:
 
     def remaining(self):
         print('The coffee machine has:')
-        for key, value in self.supply.items():
-            print(('$' if key == 'money' else '') + f'{value} of {key}')
+        for key, value in self.attribute_keys.items():
+            print(('$' if key == 'money' else '') + f'{getattr(self, key)} of {value}')
 
     def buy(self):
         check_supply = True
@@ -49,29 +55,29 @@ class CoffeeMachine:
         coffee_type = number_of_coffee_type[coffee_type]
 
         for key, value in CoffeeMachine.coffee_types[coffee_type].items():
-            if self.supply[key] - value < 0:
+            if getattr(self, key) - value < 0:
                 print(f'Sorry, not enough {key}!')
                 check_supply = False
                 break
 
         if check_supply:
             for key, value in CoffeeMachine.coffee_types[coffee_type].items():
-                self.supply[key] -= value
+                setattr(self, key, (getattr(self, key) - value))
             print('I have enough resources, making you a coffee!')
 
     def fill(self):
         print('Write how many ml of water do you want to add:')
-        self.supply['water'] += int(self.user_input())
+        self.water += int(self.user_input())
         print('Write how many ml of milk do you want to add:')
-        self.supply['milk'] += int(self.user_input())
+        self.milk += int(self.user_input())
         print('Write how many grams of coffee beans do you want to add:')
-        self.supply['coffee beans'] += int(self.user_input())
+        self.c_beans += int(self.user_input())
         print('Write how many disposable cups of coffee do you want to add:')
-        self.supply['disposable cups'] += int(self.user_input())
+        self.d_cups += int(self.user_input())
 
     def take(self):
-        print(f"I gave you ${self.supply['money']}")
-        self.supply['money'] = 0
+        print(f"I gave you ${getattr(self, 'money')}")
+        self.money = 0
 
 
 coffee_machine = CoffeeMachine(400, 540, 120, 9, 550)
